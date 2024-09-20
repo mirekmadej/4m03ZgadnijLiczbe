d04fa6fd7e7d04b9332b583950e7a4a7392e418e
Mirosław Madej <M.Madej@zsp3zamosc.pl>
2024-09-20 13:54:52 +0200

    Dodaj pliki projektów.

---
+++ 4m03ZgadnijLiczbe/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace _4m03ZgadnijLiczbe
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ 4m03ZgadnijLiczbe/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿namespace _4m03ZgadnijLiczbe
+{
+    public partial class MainPage : ContentPage
+    {
+        int wylosowana = 0;
+        int liczbaProb = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStartClicked(object sender, EventArgs e)
+        {
+            liczbaProb = 0;
+            Random r = new Random();
+            wylosowana = r.Next(0, 101);
+            btnZatwierdz.IsEnabled = true;
+            entLiczba.IsEnabled = true;
+        }
+
+
+        private void btnZatwierdzClicked(object sender, EventArgs e)
+        {
+            string s = entLiczba.Text;
+            entLiczba.Text = "";
+            int liczba;
+            if(int.TryParse(s, out liczba))
+            {
+                liczbaProb++;
+                lblProby.Text = $"Liczba prób: {liczbaProb}";
+                if(liczba == wylosowana)
+                {
+                    lblWynik.Text = "BRAWO - to jest ta liczba!";
+                    btnZatwierdz.IsEnabled = false;
+                    entLiczba.IsEnabled = false;
+                }
+                else if(liczba > wylosowana)
+                {
+                    lblWynik.Text = "za dużo";
+                }
+                else
+                    lblWynik.Text = "za mało";
+            }
+            else               
+                return;
+        }
+    }
+
+}
